031ccd7ee8ec528141a983e3775d3b79af2fa41e
EduRivera <eduardriveramestre@gmail.com>
2014-07-09 11:14:44 +0200

    Refactoring rover.js and replace advance

--- js/rover.js
+++ js/rover.js
@@ -33,23 +33,19 @@ var Rover = {
   },
   advance: function (direction) {
     direction = (direction === 'forward') ? 1 : -1;
-    var formerPosition = [
-      this.position[0],
-      this.position[1]
-    ];
+    var X = this.position[0], Y = this.position[1];
+
     var increment = this.increments[this.orientation];
-    this.position = [
-      (this.position[0] + increment[0] * direction) % Grid.sizeX,
-      (this.position[1] + increment[1] * direction) % Grid.sizeY
-    ];
-    if (this.position[0] < 0) {
-      this.position[0] = Grid.sizeX + this.position[0];
+    X = (X + increment[0] * direction) % Grid.sizeX;
+    Y = (Y + increment[1] * direction) % Grid.sizeY;
+    if (X < 0) {
+      X += Grid.sizeX;
     }
-    if (this.position[1] < 0) {
-      this.position[1] = Grid.sizeY + this.position[1];
+    if (Y < 0) {
+      Y += Grid.sizeY;
     }
-    if (Grid.thereIsObstacle(this.position[0], this.position[1])) {
-      this.position = formerPosition;
+    if (!Grid.thereIsObstacle(X, Y)) {
+      this.position = [X, Y];
     }
   },
   increments: {
